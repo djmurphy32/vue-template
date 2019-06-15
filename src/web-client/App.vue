dc3c3bb2f0d6c92b05a804b7bc2d19eecdad2123
Danny Murphy <danny.murphy@gmail.com>
2019-06-15 16:50:50 +0100

    Initial commit

---
+++ src/web-client/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>Hello {{name}}!</div>
+    <input v-model="name" type="text">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  el: "#app",
+  data: {
+    name: "World"
+  }
+});
+</script>
