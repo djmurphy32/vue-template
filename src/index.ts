e7d7e028a13e7043c86092549043511b068d0426
Danny Murphy <danny.murphy@gmail.com>
2019-06-16 00:41:58 +0100

    Working with vue component

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import Vue from "vue";
-import HelloComponent from "./components/Hello";
+import HelloComponent from "./components/Hello.vue";
 
 let v = new Vue({
     el: "#app",
     template: `
     <div>
         Name: <input v-model="name" type="text">
+        <h1>Hello Component</h1>
         <hello-component :name="name" :initialEnthusiasm="5" />
-    </div>
+        </div>
     `,
     data: { name: "World" },
     components: {
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
+
 var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/web-client/main.ts',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
