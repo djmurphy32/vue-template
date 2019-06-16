36823465011465a6f5edc5f24bbca80172896091
Danny Murphy <danny.murphy@gmail.com>
2019-06-16 10:08:56 +0100

    Migrated root vue component to SFC

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Name:
+    <input v-model="name" type="text">
+    <h1>Hello Component</h1>
+    <HelloComponent :name="name" :initialEnthusiasm="5"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloComponent from "./components/Hello.vue";
+
+export default Vue.extend({
+  name: "App",
+  components: {
+      HelloComponent
+  }
+});
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
 import Vue from "vue";
-import HelloComponent from "./components/Hello.vue";
+import App from "./App.vue";
 
-let v = new Vue({
-    el: "#app",
-    template: `
-    <div>
-        Name: <input v-model="name" type="text">
-        <h1>Hello Component</h1>
-        <hello-component :name="name" :initialEnthusiasm="5" />
-        </div>
-    `,
-    data: { name: "World" },
-    components: {
-        HelloComponent
-    }
-});
+new Vue({
+    render: (h) => h(App),
+}).$mount('#app');
